b2d16065c570bef279a2f7639bafc5d08d41232a
janneojajarvi <janne.ojajarvi@gmail.com>
2026-04-22 14:26:59 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'arvaabiisi';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './kuva.png'
+];
+
+// Asennusvaihe: Tallennetaan perustiedostot välimuistiin
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Aktivoituminen
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
