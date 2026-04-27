7ea7a0ae3a005ff29320e52cbd07922dc67bbbc5
janneojajarvi <janne.ojajarvi@gmail.com>
2026-04-27 20:03:04 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'arvaabiisi-v14'; // Päivitä aina versio, kun muutat ASSETS-listaa
+const CACHE_NAME = 'arvaabiisi-v15'; // Päivitä aina versio, kun muutat ASSETS-listaa
 
 const ASSETS = [
   './',
